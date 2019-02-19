c8fc3b129ae9fc05c022f0b4ebd0bbd2bfd50895
Moxious <moxious@oldhat.org>
2019-02-19 12:21:01 -0500

    more tests

---
+++ test/run-configuration_test.js
@@ -0,0 +1,44 @@
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+const runConfig = require('../src/run-configuration');
+const _ = require('lodash');
+
+describe('Run Configuration', function() {
+    const args = {
+        n: 10, p: 'admin', a: 'bolt://localhost', concurrency: 27,
+    };
+
+    beforeEach(() => {
+        process.env = {};
+    });
+
+    it('throws Error when missing n or ms', () => {
+        expect(() => runConfig.generateFromArgs({})).to.throw(Error);
+    });
+
+    it('throws Error when URI is missing', () => 
+        expect(() => runConfig.generateFromArgs({ n: 1 })).to.throw(Error));
+
+    it('throws Error when password is missing', () => 
+        expect(() => runConfig.generateFromArgs({ n: 1, a: 'bolt://localhost' })).to.throw(Error));
+
+    it('returns a good config with minimal input', () => {
+        const c = runConfig.generateFromArgs(args);
+        expect(c.username).to.equal('neo4j');
+        expect(c.password).to.equal(args.p);
+        expect(c.address).to.equal(args.a);
+        expect(c.runType).to.equal('counted');
+        expect(c.failFast).to.equal(false);
+        expect(c.concurrency).to.equal(args.concurrency);
+        expect(c.iterateUntil).to.be.ok;
+        expect(c.probabilityTable).to.be.ok;
+        expect(c.checkpointFreq).to.be.ok;
+    });
+
+    it('supports timed run types', () => {
+        const newArgs = _.merge({ ms: 1000 }, _.pick(args, ['p', 'a']));
+        const c = runConfig.generateFromArgs(newArgs);
+        expect(c.runType).to.equal('timed');
+        expect(c.ms).to.equal(newArgs.ms);
+    });
+});
---
+++ test/termination-condition_test.js
@@ -0,0 +1,45 @@
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+const tc = require('../src/termination-condition');
+
+describe('Termination Conditions', function () {
+    it('can make an nRuns termination condition', () => {
+        const n = tc.nRuns(3);
+
+        expect(n).to.be.ok;
+
+        // Run 1
+        let v = n.next();
+        expect(v.done).to.equal(false);
+
+        // Run 2
+        v = n.next();
+        expect(v.done).to.equal(false);
+        console.log(n.progress());
+        expect(n.progress()).to.equal(2/3);
+
+        // Run 3
+        v = n.next();
+        expect(v.done).to.equal(true);
+    });
+
+    it('can make a timeoutMilliseconds termination condition', () => {
+        const n = tc.timeoutMilliseconds(1000);
+
+        return new Promise((resolve, reject) => {
+            const p = n.next();
+            expect(p.done).to.equal(false);
+            
+            setTimeout(() => {
+                const p = n.next();
+                expect(p.done).to.equal(false);
+            }, 200);
+            
+            setTimeout(() => {
+                const p = n.next();
+                expect(p.done).to.equal(true);
+                resolve(true);
+            }, 1005);
+        });
+    });
+});

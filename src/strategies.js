108f3c52727a5de8039f7290fcfcf678b12540cc
Moxious <moxious@oldhat.org>
2019-02-26 09:04:40 -0500

    build strategy table from runConfig

--- src/strategies.js
+++ src/strategies.js
@@ -1,6 +1,7 @@
 /**
  * Library of strategies for easy inclusion.
  */
+const SimpleQueryStrategy = require('./SimpleQueryStrategy');
 const NAryTreeStrategy = require('./write-strategy/NAryTreeStrategy');
 const FatNodeAppendStrategy = require('./write-strategy/FatNodeAppendStrategy');
 const MergeWriteStrategy = require('./write-strategy/MergeWriteStrategy');
@@ -16,28 +17,60 @@ const LongPathReadStrategy = require('./read-strategy/LongPathReadStrategy');
 const RandomAccessReadStrategy = require('./read-strategy/RandomAccessReadStrategy');
 
 /**
- * Produces a strategy table
+ * Produces a strategy table by looking through the probability table and assigning
+ * only those strategies that are needed.
  * @param {SessionPool} sessionPool 
+ * @param {runConfig} a runconfig object
  * @returns {Object} key is strategy name, value is a Strategy instance object.
  */
-const builder = sessionPool => ({
-    // WRITE STRATEGIES
-    naryWrite: new NAryTreeStrategy({ n: 2, sessionPool }),
-    fatnodeWrite: new FatNodeAppendStrategy({ sessionPool }),
-    mergeWrite: new MergeWriteStrategy({ n: 1000000, sessionPool }),
-    rawWrite: new RawWriteStrategy({ n: 10, sessionPool }),
-    randomLinkage: new RandomLinkageStrategy({ n: 1000000, sessionPool }),
-    starWrite: new StarWriteStrategy({ sessionPool }),
-    indexHeavy: new IndexHeavyStrategy({ sessionPool }),
-    lockTorture: new LockTortureStrategy({ sessionPool }),
-    luceneWrite: new LuceneWriteStrategy({ sessionPool }),
+const builder = (sessionPool, runConfig) => {
+    const defaultStratTable = {
+        // WRITE STRATEGIES
+        naryWrite: new NAryTreeStrategy({ n: 2, sessionPool }),
+        fatnodeWrite: new FatNodeAppendStrategy({ sessionPool }),
+        mergeWrite: new MergeWriteStrategy({ n: 1000000, sessionPool }),
+        rawWrite: new RawWriteStrategy({ n: 10, sessionPool }),
+        randomLinkage: new RandomLinkageStrategy({ n: 1000000, sessionPool }),
+        starWrite: new StarWriteStrategy({ sessionPool }),
+        indexHeavy: new IndexHeavyStrategy({ sessionPool }),
+        lockTorture: new LockTortureStrategy({ sessionPool }),
+        luceneWrite: new LuceneWriteStrategy({ sessionPool }),
 
-    // READ STRATEGIES
-    aggregateRead: new AggregateReadStrategy({ sessionPool }),
-    metadataRead: new MetadataReadStrategy({ sessionPool }),
-    longPathRead: new LongPathReadStrategy({ sessionPool }),
-    randomAccess: new RandomAccessReadStrategy({ sessionPool }),
-});
+        // READ STRATEGIES
+        aggregateRead: new AggregateReadStrategy({ sessionPool }),
+        metadataRead: new MetadataReadStrategy({ sessionPool }),
+        longPathRead: new LongPathReadStrategy({ sessionPool }),
+        randomAccess: new RandomAccessReadStrategy({ sessionPool }),
+    };
+
+    if (runConfig) {
+        // By choosing which strats are in the table (by probability table)
+        // We're limiting which setup methods get run too.
+        const labels = runConfig.probabilityTable.getLabels();
+        const chosenTable = {};
+
+        labels.forEach(label => {
+            const strat = defaultStratTable[label];
+
+            if (label === 'custom') {
+                chosenTable[label] = new SimpleQueryStrategy({
+                    sessionPool,
+                    query: runConfig.query,
+                    mode: runConfig.mode || 'WRITE',
+                });
+                return;
+            } else if(!strat) {
+                throw new Error(`Probability table references strat ${label} which is unknown`);
+            }
+
+            chosenTable[label] = strat;
+        });
+
+        return chosenTable;
+    }
+
+    return defaultStratTable;
+};
 
 /**
  * Reports key stats about strategies
@@ -66,5 +99,5 @@ const showLastQuery = strategyTable => {
 };
 
 module.exports = {
-    builder, report, showLastQuery, 
+    builder, report, showLastQuery,
 };
--- test/strategies_test.js
+++ test/strategies_test.js
@@ -1,12 +1,19 @@
 const expect = require('chai').expect;
 const assert = require('chai').assert;
 const strategies = require('../src/strategies');
+const ProbabilityTable = require('../src/stats/ProbabilityTable');
 const Strategy = require('../src/Strategy');
 const mocks = require('./mocks');
 
 describe('Strategies', function() {
     const pool = new mocks.MockSessionPool();
     let table;
+    let mockRunConfig = {
+        probabilityTable: new ProbabilityTable([
+            [ 1.0, 'custom'],
+        ]),
+        query: "RETURN 1",
+    };
 
     beforeEach(() => {
         table = strategies.builder(pool);
@@ -16,6 +23,12 @@ describe('Strategies', function() {
         expect(table).to.be.ok;
 
         Object.values(table).forEach(strat => expect(strat).to.be.instanceOf(Strategy));
+    });
+
+    it('should build a strategy table with only things in the probability table', () => {
+        const tbl = strategies.builder(pool, mockRunConfig);
+        expect(Object.keys(tbl).length).to.equal(1);
+        expect(tbl['custom']).to.be.instanceOf(Strategy);
     });
 
     it('can report', () => {
